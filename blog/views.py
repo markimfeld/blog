699ccc59a84f0b06858bb372ce739deb074b4fc6
marcos <sebastianimfeld@gmail.com>
2020-02-07 11:03:12 -0300

    add post_detail template

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from .models import Post
 
 class PostList(generic.ListView):
 	queryset = Post.objects.filter(status=1).order_by('-pub_date')
-	template_name = 'index.html'
+	template_name = 'blog/index.html'
 
 class PostDetail(generic.DetailView):
 	model = Post
-	template_name = 'post_detail.html'
+	template_name = 'blog/post_detail.html'
 
